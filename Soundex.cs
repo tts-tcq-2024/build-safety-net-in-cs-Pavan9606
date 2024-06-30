3a387ade1597e8332cb72a305eb7a13b1999d32e
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-06-30 17:10:04 +0530

    Update Soundex.cs
    Refactoring code

--- Soundex.cs
+++ Soundex.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Text;
 
 public class Soundex
@@ -14,60 +15,53 @@ public class Soundex
         soundex.Append(char.ToUpper(name[0]));
         char prevCode = GetSoundexCode(name[0]);
         AppendGetSoundexCodes(name, soundex, ref prevCode);
-        checkSoundexAppend(soundex);
+        CheckSoundexAppend(soundex);
 
         return soundex.ToString();
     }
-    private static void AppendGetSoundexCodes(string name , StringBuilder soundex, ref char prevCode)
+
+    private static void AppendGetSoundexCodes(string name, StringBuilder soundex, ref char prevCode)
     {
-        for(int i=1;i<name.Length && soundex.Length < 4;i++)
+        for (int i = 1; i < name.Length && soundex.Length < 4; i++)
         {
             char code = GetSoundexCode(name[i]);
+            if (IsCodeValid(code, prevCode))
+            {
+                soundex.Append(code);
+                prevCode = code;
+            }
         }
     }
+
     private static bool IsCodeValid(char code, char prevCode)
     {
         return code != '0' && code != prevCode;
     }
-    private static void checkSoundexAppend(StringBuilder soundex)
+
+    private static void CheckSoundexAppend(StringBuilder soundex)
     {
         while (soundex.Length < 4)
         {
             soundex.Append('0');
         }
     }
-    
+    private static readonly Dictionary<char, char> SoundexMap = new Dictionary<char, char>
+    {
+        {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
+        {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, {'Q', '2'}, {'S', '2'}, {'X', '2'}, {'Z', '2'},
+        {'D', '3'}, {'T', '3'},
+        {'L', '4'},
+        {'M', '5'}, {'N', '5'},
+        {'R', '6'}
+    };
+
     private static char GetSoundexCode(char c)
     {
         c = char.ToUpper(c);
-        switch (c)
+        if (SoundexMap.ContainsKey(c))
         {
-            case 'B':
-            case 'F':
-            case 'P':
-            case 'V':
-                return '1';
-            case 'C':
-            case 'G':
-            case 'J':
-            case 'K':
-            case 'Q':
-            case 'S':
-            case 'X':
-            case 'Z':
-                return '2';
-            case 'D':
-            case 'T':
-                return '3';
-            case 'L':
-                return '4';
-            case 'M':
-            case 'N':
-                return '5';
-            case 'R':
-                return '6';
-            default:
-                return '0'; // For A, E, I, O, U, H, W, Y
+            return SoundexMap[c];
         }
+        return '0'; // For A, E, I, O, U, H, W, Y
     }
 }
