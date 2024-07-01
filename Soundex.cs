9ae39a9505e8e44e854d1681ba6c82f031f04c41
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-07-01 16:24:10 +0530

    Update Soundex.cs
    Updated file

--- Soundex.cs
+++ Soundex.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 public class Soundex
@@ -11,58 +13,67 @@ public class Soundex
         }
 
         StringBuilder soundex = new StringBuilder();
-        soundex.Append(char.ToUpper(name[0]));
-        char prevCode = GetSoundexCode(name[0]);
+        SoundexAppend(soundex, name[0]);
+        GetSoundexProcess(soundex, name);
 
-        for (int i = 1; i < name.Length && soundex.Length < 4; i++)
-        {
-            char code = GetSoundexCode(name[i]);
-            if (code != '0' && code != prevCode)
-            {
-                soundex.Append(code);
-                prevCode = code;
-            }
-        }
-
-        while (soundex.Length < 4)
-        {
-            soundex.Append('0');
-        }
+        checkingsoundex(soundex);
 
         return soundex.ToString();
     }
 
+    private static void SoundexAppend(StringBuilder soundex, char value)
+    {
+        soundex.Append(char.ToUpper(value));
+    }
+
+    private static void GetSoundexProcess(StringBuilder soundex, string name)
+    {
+        char prevCode = GetSoundexCode(name[0]);
+
+        foreach (var c in name.Skip(1))
+        {
+            if (soundex.Length >= 4) break;
+
+            AppendValidCode(soundex, c, ref prevCode);
+        }
+    }
+
+    private static void AppendValidCode(StringBuilder soundex, char c, ref char prevCode)
+    {
+        char code = GetSoundexCode(c);
+        if (IsValidCode(code, prevCode))
+        {
+            soundex.Append(code);
+            prevCode = code;
+        }
+    }
+
+    private static bool IsValidCode(char code, char prevCode)
+    {
+        return code != '0' && code != prevCode;
+    }
+
+    private static void checkingsoundex(StringBuilder soundex)
+    {
+        while (soundex.Length < 4)
+        {
+            soundex.Append('0');
+        }
+    }
+    private static readonly Dictionary<char, char> soundexCodeMap = new Dictionary<char, char>
+    {
+        { 'B', '1' }, { 'F', '1' }, { 'P', '1' }, { 'V', '1' },
+        { 'C', '2' }, { 'G', '2' }, { 'J', '2' }, { 'K', '2' },
+        { 'Q', '2' }, { 'S', '2' }, { 'X', '2' }, { 'Z', '2' },
+        { 'D', '3' }, { 'T', '3' },
+        { 'L', '4' },
+        { 'M', '5' }, { 'N', '5' },
+        { 'R', '6' }
+    };
+
     private static char GetSoundexCode(char c)
     {
         c = char.ToUpper(c);
-        switch (c)
-        {
-            case 'B':
-            case 'F':
-            case 'P':
-            case 'V':
-                return '1';
-            case 'C':
-            case 'G':
-            case 'J':
-            case 'K':
-            case 'Q':
-            case 'S':
-            case 'X':
-            case 'Z':
-                return '2';
-            case 'D':
-            case 'T':
-                return '3';
-            case 'L':
-                return '4';
-            case 'M':
-            case 'N':
-                return '5';
-            case 'R':
-                return '6';
-            default:
-                return '0'; // For A, E, I, O, U, H, W, Y
-        }
+        return soundexCodeMap.ContainsKey(c) ? soundexCodeMap[c] : '0';
     }
 }
